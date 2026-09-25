e89f34828f372de173d04e3bbe68b56f95b29fbb
neoaji1-svg <neoaji1@gmail.com>
2026-09-24 23:01:48 -0500

    Add Producto class with properties

---
+++ roducto.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Laboratorio4
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public byte[] Imagen { get; set; }
+    }
+}using System;
+
+namespace Laboratorio4
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public byte[] Imagen { get; set; }
+    }
+}
